f81b4af01cceb05a433148d7a72a37100c30220a
marcus fernando <marcusfcbarbosa@gmail.com>
2019-04-19 18:10:50 -0300

    Entendendo a estrutura de componentes do angular 2

---
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  eventos: any = [
+    {
+        EventoId: 1,
+      Tema: 'Angular',
+      Local: 'São Paulo'
+    },
+    {
+          EventoId: 2,
+          Tema: 'Angular',
+         Local: 'Bahia'
+     },
+     {
+         EventoId: 3,
+          Tema: 'Angular',
+          Local: 'MG'
+        },
+
+];
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
