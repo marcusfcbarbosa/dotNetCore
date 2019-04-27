1dee52952ea93d467ff66fb0e39a5c75b5afe83c
marcus fernando <marcusfcbarbosa@gmail.com>
2019-04-27 18:22:30 -0300

    Filttro de Grid, nao funcionando

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -7,22 +7,44 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+  
 
+  eventosFiltrados: any = [];
   eventos: any = [];
   imagemLargura: number = 50;
   imagemMargem: number = 2;
   mostrarImagem: boolean = false;
-  filtroLista ="";
-  
+
   constructor(private http: HttpClient) {   }
 
   ngOnInit() {
     this.getEventos();
   }
 
-    alternarImagem(){
-      this.mostrarImagem= !this.mostrarImagem;
-    }
+  _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    
+    this.filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos;
+    console.log(this.eventosFiltrados);
+  }
+ 
+
+  filtrarEventos(filtrarPor: string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+       evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
   getEventos(){
      this.http.get('http://localhost:5001/api/values').subscribe(
       response => {
